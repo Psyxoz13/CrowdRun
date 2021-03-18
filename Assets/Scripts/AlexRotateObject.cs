45fb63341a9bd37266bf439bf37e33cd118dfd04
AlexandrSemenuk <70596264+AlexandrSemenuk@users.noreply.github.com>
2021-03-18 20:58:35 +0200

    Start Alex Work

---
+++ Assets/Scripts/AlexRotateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlexRotateObject : MonoBehaviour
+{
+    [SerializeField] private float rotationFactor;
+    Quaternion rotation;
+    Quaternion originRotation;
+    
+    void Start()
+    {
+        originRotation = transform.rotation;
+    }
+        void FixedUpdate()
+    {
+        transform.rotation *= Quaternion.AngleAxis(rotationFactor * -1f, new Vector3(0, 1, 0));
+    }
+}
